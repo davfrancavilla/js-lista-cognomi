619e5a6ad322df1c2f1afe0fdfab79ebaf75584b
Davide Francavilla <davfrancavilla@gmail.com>
2020-07-13 15:02:10 +0200

    Esercizio completato

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// creo un arrei con alcuni cognomi al suo interno
+var lastNames = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
+
+// creo una variabile con il cognome scritto dall'utente
+var userLastName = prompt('Inserisci il tuo cognome');
+
+// rendo maiuscola la prima lettera e minuscole le altre (del cognome dell'utente)
+var big = userLastName.slice(0,1).toUpperCase();
+var small = userLastName.slice(1,userLastName.length).toLowerCase();
+userLastName = big + small;
+
+// inserisco il cognome dell'utente nell'array
+lastNames.push(userLastName);
+
+// ordine l'array alfabeticamente
+lastNames.sort();
+
+// calcolo la posizione "umana" del cognome dell'utente all'interno dell'array
+var humanPosition = lastNames.indexOf(userLastName) + 1;
+
+// stampo l'array ordinato
+var i;
+for (i in lastNames) {
+    document.getElementById('list').innerHTML += '<li>' + lastNames[i] + '</li>';
+}
+
+// stampo la posizione del cognome dell'utente all'interno dell'array
+document.getElementById('position').innerHTML = 'Il cognome ' + userLastName + ' si trova alla posizione ' + humanPosition;
